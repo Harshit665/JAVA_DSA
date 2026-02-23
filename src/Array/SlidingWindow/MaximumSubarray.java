99433ae6ddd8f5eee3ea8cf9b500f16773c2101f
Harshit Kesharwani <harshit91735@gmail.com>
2026-02-24 00:49:49 +0530

    maximum element of sub array

---
+++ src/Array/SlidingWindow/MaximumSubarray.java
@@ -0,0 +1,47 @@
+package Array.SlidingWindow;
+import Utility.*;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class MaximumSubarray {
+
+    // this algo is working for all condition but not working for strictly increasing order array.
+    static ArrayList<Integer> maximumElementOfSubArray(int [] arr, int k){
+        int i=0;
+        int j=0;
+        ArrayList<Integer> maximum = new ArrayList<>();
+        ArrayList<Integer> ans = new ArrayList<>();
+
+        while (j < arr.length){
+
+            if (maximum.isEmpty()){
+                maximum.add(arr[j]);
+             }
+
+            if (arr[j] > maximum.get(0)){
+                maximum.remove(0);
+                maximum.add(arr[j]);
+            }
+
+            if (j-i+1 < k){
+                j++;
+            }
+
+            if (j-i+1 == k){
+                ans.add(maximum.get(0));
+                i++;
+                j++;
+            }
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = InputArray.inputArray();
+        Scanner sc = new Scanner(System.in);
+        System.out.println("window size");
+        int k = sc.nextInt();
+        ArrayList<Integer> ans = maximumElementOfSubArray(arr,k);
+        System.out.println(ans);
+    }
+}
