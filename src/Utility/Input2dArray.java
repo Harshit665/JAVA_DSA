af1d3f2bbd11b7f994289af97075ac6ed54631d9
Harshit Kesharwani <harshit91735@gmail.com>
2026-02-25 11:27:02 +0530

    introduction to 2D Array

---
+++ src/Utility/Input2dArray.java
@@ -0,0 +1,26 @@
+package Utility;
+
+import java.util.Scanner;
+
+public class Input2dArray {
+
+    public static int[][] input2dArray(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter length of first array or ROW");
+        int row = sc.nextInt();
+        System.out.println("enter length of column or COLUMN");
+        int col = sc.nextInt();
+        int[][] arr = new int[row][col];
+        for (int i =0;i<row;i++){
+            for (int j=0;j<col;j++){
+                arr[i][j] = sc.nextInt();
+            }
+        }
+        return arr;
+    }
+
+    public static void main(String[] args) {
+        int[][] ans = input2dArray();
+
+    }
+}
---
+++ src/Utility/Print2dArray.java
@@ -0,0 +1,12 @@
+package Utility;
+
+public class Print2dArray {
+
+    public static void print2dArray(int[][] arr){
+        for (int i=0;i<arr.length;i++){
+            for (int j=0;j<arr[i].length;j++){
+                System.out.println(arr[i][j]);
+            }
+        }
+    }
+}
