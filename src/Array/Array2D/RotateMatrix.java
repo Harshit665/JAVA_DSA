135278e2ecb2a66722c57be2fdcdb41f1913e9c5
Harshit Kesharwani <harshit91735@gmail.com>
2026-02-26 12:30:12 +0530

    rotation of a matrix

---
+++ src/Array/Array2D/RotateMatrix.java
@@ -0,0 +1,13 @@
+package Array.Array2D;
+
+public class RotateMatrix {
+
+    static void rotateMatrix(int[][] matrix){
+        // transpose the matrix
+        //reverse each row of the matrix like 1D array
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
