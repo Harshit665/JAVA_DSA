d8c509c502fa0bfc4a7731b70e268c07b9c4b3f6
Harshit Kesharwani <harshit91735@gmail.com>
2026-02-28 14:50:44 +0530

    sum function

--- src/Array/Array2D/RectangleSum.java
+++ src/Array/Array2D/RectangleSum.java
@@ -4,9 +4,16 @@ import java.util.Scanner;
 
 public class RectangleSum {
 
-//    static int findSum(int[][] matrix,int l1,int r1,int l2,int r2){
-//        int sum = 0;
-//    }
+    static int findSum(int[][] matrix,int l1,int r1,int l2,int r2){
+        int sum = 0;
+
+        for (int i =l1;i<=l2;i++){
+            for (int j=r1;j<r2;j++){
+                sum += matrix[i][j];
+            }
+        }
+        return sum;
+    }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -21,6 +28,12 @@ public class RectangleSum {
                 matrix[i][j] = sc.nextInt();
             }
         }
-        System.out.println();
+        System.out.println("enter rectangle bounderies l1 ,r1, l2, r2");
+        int l1 = sc.nextInt();
+        int r1 = sc.nextInt();
+        int l2 = sc.nextInt();
+        int r2 = sc.nextInt();
+
+        System.out.println("rectangle Sum " + findSum(matrix,l1,r1,l2,r2));
     }
 }
